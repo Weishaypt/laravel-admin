1c362a00fe63e60425e6438ec2376153b3e35927
Alexandr Konyaev <40069228+Weishaypt@users.noreply.github.com>
2021-11-16 21:20:23 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,98 +6,86 @@
   <title>{{config('admin.title')}} | {{ trans('admin.login') }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  
+
   @if(!is_null($favicon = Admin::favicon()))
-  <link rel="shortcut icon" href="{{$favicon}}">
+    <link rel="shortcut icon" href="{{$favicon}}">
   @endif
 
-  <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
+  <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/admin/app.css") }}">
+  <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/admin/bundle.css") }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/font-awesome/css/font-awesome.min.css") }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/AdminLTE/dist/css/AdminLTE.min.css") }}">
   <!-- iCheck -->
   <link rel="stylesheet" href="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/iCheck/square/blue.css") }}">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+  
 </head>
-<body class="hold-transition login-page" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
-<div class="login-box">
-  <div class="login-logo">
-    <a href="{{ admin_url('/') }}"><b>{{config('admin.name')}}</b></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">{{ trans('admin.login') }}</p>
+<body class="form-membership dark">
+<style>
+  #logo img {
+    display: block !important;
+  }
+</style>
 
-    <form action="{{ admin_url('auth/login') }}" method="post">
-      <div class="form-group has-feedback {!! !$errors->has('username') ?: 'has-error' !!}">
-
-        @if($errors->has('username'))
-          @foreach($errors->get('username') as $message)
-            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label><br>
-          @endforeach
-        @endif
-
-        <input type="text" class="form-control" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username') }}">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback {!! !$errors->has('password') ?: 'has-error' !!}">
-
-        @if($errors->has('password'))
-          @foreach($errors->get('password') as $message)
-            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label><br>
-          @endforeach
-        @endif
-
-        <input type="password" class="form-control" placeholder="{{ trans('admin.password') }}" name="password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          @if(config('admin.auth.remember'))
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox" name="remember" value="1" {{ (!old('username') || old('remember')) ? 'checked' : '' }}>
-              {{ trans('admin.remember_me') }}
-            </label>
-          </div>
-          @endif
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('admin.login') }}</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-
-  </div>
-  <!-- /.login-box-body -->
+<!-- begin::preloader-->
+<div class="preloader">
+  <div class="preloader-icon"></div>
 </div>
-<!-- /.login-box -->
+<!-- end::preloader -->
+
+<div class="form-wrapper">
+
+  <!-- logo -->
+  <div id="logo">
+    <a class="d-flex justify-content-center align-items-center font-weight-bold" href="{{ admin_url('/') }}">
+      {!! config('admin.logo', config('admin.name')) !!}
+    </a>
+  </div>
+  <!-- ./ logo -->
+
+
+  <h5>{{ trans('admin.login') }}</h5>
+
+  <form action="{{ admin_url('auth/login') }}" method="post">
+    <div class="form-group">
+
+      <input type="text" class="form-control mb-0 {!! !$errors->has('username') ?: 'is-invalid' !!}" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username') }}">
+      @if($errors->has('username'))
+        @foreach($errors->get('username') as $message)
+          <div class="invalid-feedback text-left">
+            {{$message}}
+          </div>
+        @endforeach
+      @endif
+    </div>
+    <div class="form-group {!! !$errors->has('password') ?: 'has-error' !!}">
+      <input type="password" class="form-control mb-0 {!! !$errors->has('username') ?: 'is-invalid' !!}" placeholder="{{ trans('admin.password') }}" name="password">
+      @if($errors->has('password'))
+        @foreach($errors->get('password') as $message)
+          <div class="invalid-feedback text-left">
+            {{$message}}
+          </div>
+        @endforeach
+      @endif
+    </div>
+    @if(config('admin.auth.remember'))
+      <div class="form-group d-flex justify-content-between">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" name="remember" class="custom-control-input" value="1" {{ (!old('username') || old('remember')) ? 'checked' : '' }} id="customCheck1">
+          <label class="custom-control-label" for="customCheck1"> {{ trans('admin.remember_me') }}</label>
+        </div>
+      </div>
+    @endif
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <button type="submit" class="btn btn-primary btn-block">{{ trans('admin.login') }}</button>
+  </form>
+  <!-- ./ form -->
+</div>
 
 <!-- jQuery 2.1.4 -->
-<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js")}} "></script>
-<!-- Bootstrap 3.3.5 -->
-<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/bootstrap/js/bootstrap.min.js")}}"></script>
-<!-- iCheck -->
-<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/iCheck/icheck.min.js")}}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+<script src="{{ admin_asset("vendor/laravel-admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js")}}"></script>
+<script src="{{ admin_asset("vendor/laravel-admin/admin/bundle.js")}}"></script>
+<script src="{{ admin_asset("vendor/laravel-admin/admin/app.min.js")}}"></script>
 </body>
 </html>
