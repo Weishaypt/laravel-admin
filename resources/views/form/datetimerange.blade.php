8fb2a21699b776a705fe73d19e759da5861a65fa
Alexandr Konyaev <40069228+Weishaypt@users.noreply.github.com>
2022-03-01 15:36:08 +0100

    Update datetimerange.blade.php

--- resources/views/form/datetimerange.blade.php
+++ resources/views/form/datetimerange.blade.php
@@ -1,4 +1,4 @@
-<div class="{{$viewClass['form-group']}} {!! ($errors->has($errorKey['start']) || $errors->has($errorKey['end'])) ? 'has-error' : ''  !!}">
+<div class="row {{$viewClass['form-group']}} {!! ($errors->has($errorKey['start']) || $errors->has($errorKey['end'])) ? 'has-error' : ''  !!}">
 
     <label for="{{$id['start']}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
 
