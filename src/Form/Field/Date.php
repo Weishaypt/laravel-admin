045c33d54be2e647d1a3dda17cf74f1c34337004
Alexandr Konyaev <40069228+Weishaypt@users.noreply.github.com>
2022-03-02 14:16:22 +0100

    Update Date.php

--- src/Form/Field/Date.php
+++ src/Form/Field/Date.php
@@ -37,8 +37,6 @@ class Date extends Text
         $this->options['locale'] = array_key_exists('locale', $this->options) ? $this->options['locale'] : config('app.locale');
         $this->options['allowInputToggle'] = true;
 
-        $this->script = "$('{$this->getElementClassSelector()}').parent().datetimepicker(".json_encode($this->options).');';
-
         $this->prepend('<i class="fa fa-calendar fa-fw"></i>')
             ->defaultAttribute('style', 'width: 110px');
 
