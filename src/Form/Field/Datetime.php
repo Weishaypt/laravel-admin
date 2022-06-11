d58152476b8761140a80bb92e674a0b40e3128ab
Aleksandr Koniaev <ya@sanek.dev>
2022-06-11 15:39:08 +0200

    Update Datetime.php

--- src/Form/Field/Datetime.php
+++ src/Form/Field/Datetime.php
@@ -6,6 +6,18 @@ class Datetime extends Date
 {
     protected $format = 'YYYY-MM-DD HH:mm';
 
+    public function prepare($value)
+    {
+        if ($value === '') {
+            $value = null;
+        }
+        else {
+            $value = date('c', strtotime($value));
+        }
+
+        return $value;
+    }
+
     public function render()
     {
         $this->defaultAttribute('style', 'width: 160px');
